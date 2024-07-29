83c69b8113bc0a83e3221ca241560efae3cfb2e2
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2024-07-29 16:52:33 +0200

    Better background viewer

--- platforms/shared/desktop/gui_debug_huc6270.cpp
+++ platforms/shared/desktop/gui_debug_huc6270.cpp
@@ -170,6 +170,8 @@ void gui_debug_window_huc6270_registers(void)
     ImGui::PopStyleVar();
 }
 
+static const float k_scale_levels[3] = { 1.0f, 1.5f, 2.0f };
+
 void gui_debug_window_huc6270_background(void)
 {
     ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 8.0f);
@@ -177,16 +179,140 @@ void gui_debug_window_huc6270_background(void)
     ImGui::SetNextWindowSize(ImVec2(401, 641), ImGuiCond_FirstUseEver);
     ImGui::Begin("HuC6270 Background", &config_debug.show_huc6270_background);
 
-    ImGui::PushFont(gui_default_font);
-
     GeargrafxCore* core = emu_get_core();
     HuC6270* huc6270 = core->GetHuC6270();
+    HuC6270::HuC6270_State* huc6270_state = huc6270->GetState();
+    u16* vram = huc6270->GetVRAM();
+    int screen_reg = (huc6270_state->R[HUC6270_REG_MWR] >> 4) & 0x07;
+    int screen_size_x = k_scren_size_x[screen_reg];
+    int screen_size_y = k_scren_size_y[screen_reg];
+    int bat_size = screen_size_x * screen_size_y;
 
-    ImGui::TextColored(magenta, "BACKGROUND");
+    static bool show_grid = true;
+    static int zoom = 2;
+    ImVec4 grid_color = dark_gray;
+    grid_color.w = 0.3f;
+    float scale = k_scale_levels[zoom];
+    float size_h = 8.0f * screen_size_x * scale;
+    float size_v = 8.0f * screen_size_y * scale;
+    float spacing_h = 8.0f * scale;
+    float spacing_v = 8.0f * scale;
+    float texture_size_h = 1024.0f;
+    float texture_size_v = 512.0f;
 
-    ImGui::Image((void*)(intptr_t)renderer_emu_debug_huc6270_background, ImVec2(emu_debug_background_buffer_width, emu_debug_background_buffer_height), ImVec2(0.0f, 0.0f), ImVec2(emu_debug_background_buffer_width / 1024.0f, emu_debug_background_buffer_height / 512.0f));
+    if (ImGui::BeginTable("regs", 2, ImGuiTableFlags_BordersInnerH |ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_NoPadOuterX))
+    {
+        ImGui::TableSetupColumn("one", ImGuiTableColumnFlags_WidthFixed, 200.0f);
+        ImGui::TableSetupColumn("two", ImGuiTableColumnFlags_WidthFixed); 
 
-    ImGui::PopFont();
+        ImGui::TableNextColumn();
+
+        ImGui::PushItemWidth(60.0f);
+        ImGui::Combo("Zoom##zoom_bg", &zoom, "1x\0""1.5x\0""2x\0\0");
+        ImGui::Checkbox("Show Grid##grid_bg", &show_grid);
+
+        ImGui::TableNextColumn();
+        ImGui::PushFont(gui_default_font);
+
+        ImGui::TextColored(magenta, "ENABLED  ");
+
+        ImGui::SameLine();
+        ImGui::TextColored(huc6270_state->R[HUC6270_REG_CR] & 0x0080 ? green : gray, "%s", huc6270_state->R[HUC6270_REG_CR] & 0x0080 ? "YES" : "NO");
+
+        ImGui::SameLine();
+        ImGui::TextColored(magenta, "        SCREEN");
+
+        ImGui::SameLine();
+        ImGui::TextColored(white, "%dx%d", k_scren_size_x[(huc6270_state->R[HUC6270_REG_MWR] >> 4) & 0x07], k_scren_size_y[(huc6270_state->R[HUC6270_REG_MWR] >> 4) & 0x07]);
+
+        ImGui::TextColored(magenta, "SCROLL X "); 
+
+        ImGui::SameLine();
+        ImGui::TextColored(white, "%02X (%03d)", huc6270_state->R[HUC6270_REG_BXR], huc6270_state->R[HUC6270_REG_BXR]);
+
+        ImGui::TextColored(magenta, "SCROLL Y "); 
+
+        ImGui::SameLine();
+        ImGui::TextColored(white, "%02X (%03d)", huc6270_state->R[HUC6270_REG_BYR], huc6270_state->R[HUC6270_REG_BYR]);
+
+        ImGui::PopFont();
+
+        ImGui::EndTable();
+    }
+
+    ImGui::Separator();
+
+
+    if (ImGui::BeginChild("##bg", ImVec2(0.0f, 0.0f), ImGuiChildFlags_None, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoNav))
+    {
+        ImVec2 p = ImGui::GetCursorScreenPos();
+        ImDrawList* draw_list = ImGui::GetWindowDrawList();
+        ImGuiIO& io = ImGui::GetIO();
+
+        ImGui::Image((void*)(intptr_t)renderer_emu_debug_huc6270_background, ImVec2(size_h, size_v), ImVec2(0.0f, 0.0f), ImVec2(emu_debug_background_buffer_width / texture_size_h, emu_debug_background_buffer_height / texture_size_v));
+
+        if (ImGui::IsItemHovered())
+        {
+            ImVec2 mouse_pos = ImGui::GetMousePos();
+            ImVec2 rel_pos = ImVec2((mouse_pos.x - p.x) / scale, (mouse_pos.y - p.y) / scale);
+            int x = (int)(rel_pos.x / 8.0f);
+            int y = (int)(rel_pos.y / 8.0f);
+            int i = (screen_size_x * y) + x;
+            if (i >= 0 && i < bat_size)
+            {
+                u16 bat_entry = vram[i];
+                int tile_index = bat_entry & 0x07FF;
+                int color_table = (bat_entry >> 12) & 0x0F;
+                ImGui::BeginTooltip();
+
+                float tile_scale = 16.0f;
+                float tile_width = 8.0f * tile_scale;
+                float tile_height = 8.0f * tile_scale;
+                float tile_uv_h = (i % screen_size_x) * 8;
+                float tile_uv_v = (i / screen_size_x) * 8;
+
+                // Show tile image:
+                ImGui::Image((void*)(intptr_t)renderer_emu_debug_huc6270_background, ImVec2(tile_width, tile_height), ImVec2(tile_uv_h / texture_size_h, tile_uv_v / texture_size_v), ImVec2((tile_uv_h + 8) / texture_size_h, (tile_uv_v + 8) / texture_size_v));
+
+                ImGui::PushFont(gui_default_font);
+
+                ImGui::TextColored(orange, "TILE INDEX   ");
+                ImGui::SameLine();
+                ImGui::TextColored(white, "%03X", tile_index);
+
+                ImGui::TextColored(orange, "TILE ADDRESS ");
+                ImGui::SameLine();
+                ImGui::TextColored(white, "%03X", tile_index * 16);
+
+                ImGui::TextColored(orange, "COLOR TABLE  ");
+                ImGui::SameLine();
+                ImGui::TextColored(white, "%01X", color_table);
+
+                ImGui::PopFont();
+
+                ImGui::EndTooltip();
+            }
+        }
+
+        if (show_grid)
+        {
+            float x = p.x;
+            for (int n = 0; n <= screen_size_x; n++)
+            {
+                draw_list->AddLine(ImVec2(x, p.y), ImVec2(x, p.y + size_v), ImColor(grid_color), 1.0f);
+                x += spacing_h;
+            }
+
+            float y = p.y;  
+            for (int n = 0; n <= screen_size_y; n++)
+            {
+                draw_list->AddLine(ImVec2(p.x, y), ImVec2(p.x + size_h, y), ImColor(grid_color), 1.0f);
+                y += spacing_v;
+            }
+        }
+
+        ImGui::EndChild();
+    }
 
     ImGui::End();
     ImGui::PopStyleVar();
