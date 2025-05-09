c81c6a270c225030539cfaf8c5be6ae36450d353
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2025-05-09 10:46:16 +0200

    Improve about window

--- platforms/shared/desktop/gui_popups.cpp
+++ platforms/shared/desktop/gui_popups.cpp
@@ -31,6 +31,11 @@
 #include "renderer.h"
 #include "keyboard.h"
 
+static char build_info[4096] = "";
+static int info_pos = 0;
+
+static void add_build_info(const char* fmt, ...);
+
 void gui_popup_modal_keyboard()
 {
     if (ImGui::BeginPopupModal("Keyboard Configuration", NULL, ImGuiWindowFlags_AlwaysAutoResize))
@@ -107,97 +112,108 @@ void gui_popup_modal_about(void)
         {
             if (ImGui::BeginTabItem("Build Info"))
             {
-                ImGui::BeginChild("build", ImVec2(0, 100), false, ImGuiWindowFlags_AlwaysVerticalScrollbar);
+                build_info[0] = '\0';
+                info_pos = 0;
 
-                ImGui::Text("Build: %s", GG_VERSION);
-
+                add_build_info("Build: %s\n", GG_VERSION);
                 #if defined(__DATE__) && defined(__TIME__)
-                ImGui::Text("Built on: %s - %s", __DATE__, __TIME__);
+                add_build_info("Built on: %s - %s\n", __DATE__, __TIME__);
                 #endif
                 #if defined(_M_ARM64)
-                ImGui::Text("Windows ARM64 build");
+                add_build_info("Windows ARM64 build\n");
                 #endif
                 #if defined(_M_X64)
-                ImGui::Text("Windows 64 bit build");
+                add_build_info("Windows 64 bit build\n");
                 #endif
                 #if defined(_M_IX86)
-                ImGui::Text("Windows 32 bit build");
+                add_build_info("Windows 32 bit build\n");
                 #endif
                 #if defined(__linux__) && defined(__x86_64__)
-                ImGui::Text("Linux 64 bit build");
+                add_build_info("Linux 64 bit build\n");
                 #endif
                 #if defined(__linux__) && defined(__i386__)
-                ImGui::Text("Linux 32 bit build");
+                add_build_info("Linux 32 bit build\n");
                 #endif
                 #if defined(__linux__) && defined(__arm__)
-                ImGui::Text("Linux ARM build");
+                add_build_info("Linux ARM build\n");
                 #endif
                 #if defined(__linux__) && defined(__aarch64__)
-                ImGui::Text("Linux ARM64 build");
+                add_build_info("Linux ARM64 build\n");
                 #endif
                 #if defined(__APPLE__) && defined(__arm64__ )
-                ImGui::Text("macOS build (Apple Silicon)");
+                add_build_info("macOS build (Apple Silicon)\n");
                 #endif
                 #if defined(__APPLE__) && defined(__x86_64__)
-                ImGui::Text("macOS build (Intel)");
+                add_build_info("macOS build (Intel)\n");
                 #endif
                 #if defined(__ANDROID__)
-                ImGui::Text("Android build");
+                add_build_info("Android build\n");
                 #endif
+                add_build_info("Config file: %s\n", config_emu_file_path);
+                add_build_info("ImGui file: %s\n", config_imgui_file_path);
+                add_build_info("Savestate version: %d\n", GG_SAVESTATE_VERSION);
                 #if defined(_MSC_FULL_VER)
-                ImGui::Text("Microsoft C++ %d", _MSC_FULL_VER);
+                add_build_info("Microsoft C++ %d\n", _MSC_FULL_VER);
                 #endif
                 #if defined(_MSVC_LANG)
-                ImGui::Text("MSVC %d", _MSVC_LANG);
+                add_build_info("MSVC %d\n", _MSVC_LANG);
                 #endif
                 #if defined(__CLR_VER)
-                ImGui::Text("CLR version: %d", __CLR_VER);
+                add_build_info("CLR version: %d\n", __CLR_VER);
                 #endif
                 #if defined(__MINGW32__)
-                ImGui::Text("MinGW 32 bit (%d.%d)", __MINGW32_MAJOR_VERSION, __MINGW32_MINOR_VERSION);
+                add_build_info("MinGW 32 bit (%d.%d)\n", __MINGW32_MAJOR_VERSION, __MINGW32_MINOR_VERSION);
                 #endif
                 #if defined(__MINGW64__)
-                ImGui::Text("MinGW 64 bit (%d.%d)", __MINGW64_VERSION_MAJOR, __MINGW64_VERSION_MINOR);
+                add_build_info("MinGW 64 bit (%d.%d)\n", __MINGW64_VERSION_MAJOR, __MINGW64_VERSION_MINOR);
                 #endif
                 #if defined(__GNUC__) && !defined(__llvm__) && !defined(__INTEL_COMPILER)
-                ImGui::Text("GCC %d.%d.%d", (int)__GNUC__, (int)__GNUC_MINOR__, (int)__GNUC_PATCHLEVEL__);
+                add_build_info("GCC %d.%d.%d\n", (int)__GNUC__, (int)__GNUC_MINOR__, (int)__GNUC_PATCHLEVEL__);
                 #endif
                 #if defined(__clang_version__)
-                ImGui::Text("Clang %s", __clang_version__);
+                add_build_info("Clang %s\n", __clang_version__);
                 #endif
-                ImGui::Text("SDL %d.%d.%d (build)", application_sdl_build_version.major, application_sdl_build_version.minor, application_sdl_build_version.patch);
-                ImGui::Text("SDL %d.%d.%d (link) ", application_sdl_link_version.major, application_sdl_link_version.minor, application_sdl_link_version.patch);
-                ImGui::Text("OpenGL %s", renderer_opengl_version);
+                add_build_info("SDL %d.%d.%d (build)\n", application_sdl_build_version.major, application_sdl_build_version.minor, application_sdl_build_version.patch);
+                add_build_info("SDL %d.%d.%d (link)\n", application_sdl_link_version.major, application_sdl_link_version.minor, application_sdl_link_version.patch);
+                add_build_info("OpenGL %s\n", renderer_opengl_version);
                 #if !defined(__APPLE__)
-                ImGui::Text("GLEW %s", renderer_glew_version);
+                add_build_info("GLEW %s\n", renderer_glew_version);
                 #endif
-                ImGui::Text("Dear ImGui %s (%d)", IMGUI_VERSION, IMGUI_VERSION_NUM);
+                add_build_info("Dear ImGui %s (%d)\n", IMGUI_VERSION, IMGUI_VERSION_NUM);
 
                 #if defined(DEBUG)
-                ImGui::Text("define: DEBUG");
+                add_build_info("define: DEBUG\n");
+                #endif
+                #if defined(NDEBUG)
+                add_build_info("define: NDEBUG\n");
                 #endif
                 #if defined(GG_DEBUG)
-                ImGui::Text("define: GG_DEBUG");
+                add_build_info("define: GG_DEBUG\n");
                 #endif
                 #if defined(GG_NO_OPTIMIZATIONS)
-                ImGui::Text("define: GG_NO_OPTIMIZATIONS");
+                add_build_info("define: GG_NO_OPTIMIZATIONS\n");
+                #endif
+                #if defined(GG_DISABLE_DISASSEMBLER)
+                add_build_info("define: GG_DISABLE_DISASSEMBLER\n");
                 #endif
                 #if defined(__cplusplus)
-                ImGui::Text("define: __cplusplus = %d", (int)__cplusplus);
+                add_build_info("define: __cplusplus = %d\n", (int)__cplusplus);
                 #endif
                 #if defined(__STDC__)
-                ImGui::Text("define: __STDC__ = %d", (int)__STDC__);
+                add_build_info("define: __STDC__ = %d\n", (int)__STDC__);
                 #endif
                 #if defined(__STDC_VERSION__)
-                ImGui::Text("define: __STDC_VERSION__ = %d", (int)__STDC_VERSION__);
+                add_build_info("define: __STDC_VERSION__ = %d\n", (int)__STDC_VERSION__);
                 #endif
                 #if defined(GG_LITTLE_ENDIAN)
-                ImGui::Text("define: GG_LITTLE_ENDIAN");
+                add_build_info("define: GG_LITTLE_ENDIAN");
                 #endif
                 #if defined(GG_BIG_ENDIAN)
-                ImGui::Text("define: GG_BIG_ENDIAN");
+                add_build_info("define: GG_BIG_ENDIAN");
                 #endif
-                ImGui::EndChild();
+
+                ImGui::InputTextMultiline("##build_info", build_info, sizeof(build_info), ImVec2(-1.0f, 100.0f), ImGuiInputTextFlags_ReadOnly);
+
                 ImGui::EndTabItem();
             }
             if (ImGui::BeginTabItem("Special thanks to"))
@@ -269,4 +285,18 @@ void gui_show_fps(void)
     ImGui::Text("FPS:  %.2f\nTIME: %.2f ms", ImGui::GetIO().Framerate, 1000.0f / ImGui::GetIO().Framerate);
     ImGui::PopStyleColor();
     ImGui::PopFont();
+}
+
+static void add_build_info(const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    int written = vsnprintf(build_info + info_pos, sizeof(build_info) - info_pos, fmt, args);
+    if (written > 0 && info_pos + written < (int)sizeof(build_info)) {
+        info_pos += written;
+    } else {
+        info_pos = (int)sizeof(build_info) - 1;
+        build_info[info_pos] = '\0';
+    }
+    va_end(args);
 }
