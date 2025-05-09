fbe548aebf1e17b7d77c35b5f4256f7c0ed2ac4f
Tatsuya79 <jeanpascalmeunier@hotmail.com>
2025-05-09 16:08:07 +0200

    Update libretro.cpp

--- platforms/libretro/libretro.cpp
+++ platforms/libretro/libretro.cpp
@@ -532,7 +532,7 @@ static void set_variabless(void)
         { "geargrafx_overscan", "Overscan; Disabled|Enabled" },
         { "geargrafx_scanline_count", "Scanline Count; 224p|240p|Manual" },
         { "geargrafx_scanline_start", "Scanline Start (Manual); 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30" },
-        { "geargrafx_scanline_end", "Scanline End (Manual); 220|221|222|223|224|225|226|227|228|229|230|231|232|233|234|235|236|237|238|239|240|241|242" },
+        { "geargrafx_scanline_end", "Scanline End (Manual); 220|221|222|223|224|225|226|227|228|229|230|231|232|233|234|235|236|237|238|239|240|241" },
         { "geargrafx_composite_colors", "Composite Colors; Disabled|Enabled" },
         { "geargrafx_backup_ram", "Backup RAM (restart); Enabled|Disabled" },
         { "geargrafx_force_pce_jap", "Force Japanese PC Engine (restart); Disabled|Enabled" },
