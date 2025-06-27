eff0f43ac038b4dcab22903e8cdee258670751c0
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2025-06-27 11:19:43 +0200

    Update cli help

--- platforms/shared/desktop/main.cpp
+++ platforms/shared/desktop/main.cpp
@@ -86,8 +86,9 @@ int main(int argc, char* argv[])
 
     if (show_usage)
     {
-        printf("Usage: %s [options] [rom_file] [symbol_file]\n", argv[0]);
-        printf("Options:\n");
+        printf("Usage: %s [options] [game_file] [symbol_file]\n", argv[0]);
+        printf("  [game_file]         Game file: accepts ROMs (.pce, .sgx), CUE (.cue) or ZIP (.zip)\n");
+        printf("\nOptions:\n");
         printf("  -f, --fullscreen    Start in fullscreen mode\n");
         printf("  -w, --windowed      Start in windowed mode with menu visible\n");
         printf("  -v, --version       Display version information\n");
