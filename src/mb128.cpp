d53104edd93c8a111fc8d05a90ffc0fb1e30b8b5
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2025-12-08 01:53:02 +0100

    Inline MB128 code. #73

--- src/mb128.cpp
+++ src/mb128.cpp
@@ -49,57 +49,6 @@ void MB128::Reset()
     m_retval = 0;
 }
 
-void MB128::Connect(bool connected)
-{
-    m_connected = connected;
-    if (!m_connected)
-        Reset();
-}
-
-void MB128::Write(u8 value)
-{
-    if (!m_connected)
-    {
-        m_prev_data = value;
-        return;
-    }
-
-    bool old_clr = IS_SET_BIT(m_prev_data, 1);
-    bool new_sel = IS_SET_BIT(value, 0);
-    bool new_clr = IS_SET_BIT(value, 1);
-
-    // Rising edge of CLR
-    if (!old_clr && new_clr)
-    {
-        if (m_active)
-            SendBit(new_sel);
-        else
-        {
-            // Detect the 0xA8 pattern.
-            m_shiftreg = (m_shiftreg >> 1) | (new_sel ? 0x80 : 0x00);
-
-            if (m_shiftreg == 0xA8)
-            {
-                m_state = MODE_A1;
-                m_active = true;
-            }
-        }
-    }
-
-    m_prev_data = value;
-}
-
-u8 MB128::Read()
-{
-    if (!m_connected)
-        return 0;
-
-    if (!m_active)
-        return 0;
-
-    return m_retval & 0x0F;
-}
-
 void MB128::SendBit(bool sel_bit)
 {
     u8 temp_byte;
--- src/mb128_inline.h
+++ src/mb128_inline.h
@@ -42,4 +42,55 @@ INLINE const u8* MB128::GetRAM() const
     return m_ram;
 }
 
+INLINE void MB128::Connect(bool connected)
+{
+    m_connected = connected;
+    if (!m_connected)
+        Reset();
+}
+
+INLINE void MB128::Write(u8 value)
+{
+    if (!m_connected)
+    {
+        m_prev_data = value;
+        return;
+    }
+
+    bool old_clr = IS_SET_BIT(m_prev_data, 1);
+    bool new_sel = IS_SET_BIT(value, 0);
+    bool new_clr = IS_SET_BIT(value, 1);
+
+    // Rising edge of CLR
+    if (!old_clr && new_clr)
+    {
+        if (m_active)
+            SendBit(new_sel);
+        else
+        {
+            // Detect the 0xA8 pattern.
+            m_shiftreg = (m_shiftreg >> 1) | (new_sel ? 0x80 : 0x00);
+
+            if (m_shiftreg == 0xA8)
+            {
+                m_state = MODE_A1;
+                m_active = true;
+            }
+        }
+    }
+
+    m_prev_data = value;
+}
+
+INLINE u8 MB128::Read()
+{
+    if (!m_connected)
+        return 0;
+
+    if (!m_active)
+        return 0;
+
+    return m_retval & 0x0F;
+}
+
 #endif /* MB12_INLINE_H */
