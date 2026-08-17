782a7f45624dd726e3fb6051661166682c684d09
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-08-17 12:59:34 +0200

    Fix shared Base64 encoding and filename handling

--- platforms/shared/desktop/utils.h
+++ platforms/shared/desktop/utils.h
@@ -83,6 +83,31 @@ static inline int get_reset_value(int option)
     }
 }
 
+static inline std::string base64_encode(const unsigned char* data, int size)
+{
+    static const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    std::string result;
+    result.reserve(((size + 2) / 3) * 4);
+
+    int i = 0;
+    while (i < size)
+    {
+        unsigned char byte1 = data[i++];
+        bool has_byte2 = i < size;
+        unsigned char byte2 = has_byte2 ? data[i++] : 0;
+        bool has_byte3 = i < size;
+        unsigned char byte3 = has_byte3 ? data[i++] : 0;
+
+        result.push_back(base64_chars[byte1 >> 2]);
+        result.push_back(base64_chars[((byte1 & 0x03) << 4) | (byte2 >> 4)]);
+        result.push_back(has_byte2 ? base64_chars[((byte2 & 0x0F) << 2) | (byte3 >> 6)] : '=');
+        result.push_back(has_byte3 ? base64_chars[byte3 & 0x3F] : '=');
+    }
+
+    return result;
+}
+
 static inline bool get_local_time(time_t timestamp, struct tm* time_info)
 {
 #if defined(_WIN32)
